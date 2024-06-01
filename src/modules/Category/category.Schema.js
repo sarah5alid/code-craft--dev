80be9c3ba63f25452910594f4bb7b5297b3d7b33
Esraa Ali <esraaali.bis@gmail.com>
2024-06-01 18:55:27 +0300

    Increase max category name chars to 35.

--- src/modules/Category/category.Schema.js
+++ src/modules/Category/category.Schema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const addCategorySchema = {
   body: joi.object({
-    name: joi.string().min(5).max(30).required(),
+    name: joi.string().min(5).max(35).required(),
   }),
 };
 
